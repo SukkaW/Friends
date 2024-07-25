5f58ff7019994414e68a42578da9883f50efd69a
SukkaW <isukkaw@gmail.com>
2024-07-25 16:01:10 +0800

    chore: drop bun

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import yaml from 'js-yaml';
+import fs from 'fs';
 
 // Get document, or throw exception on error
-(async () => {
+(() => {
   try {
-    const doc = yaml.load(await Bun.file('./src/links.yml').text());
-    Bun.write('./dist/links.json', JSON.stringify(doc));
+    const doc = yaml.load(fs.readFileSync('./src/links.yml', 'utf-8'));
+    fs.writeFileSync('./dist/links.json', JSON.stringify(doc));
   } catch (e) {
     console.error(e);
   }
