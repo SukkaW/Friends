567d837bd89272218fd2a44f5f9737d7e6353730
SukkaW <isukkaw@gmail.com>
2024-07-25 16:01:55 +0800

    chore: housekeeping

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import fs from 'fs';
 (() => {
   try {
     const doc = yaml.load(fs.readFileSync('./src/links.yml', 'utf-8'));
+    fs.mkdirSync('./dist', { recursive: true });
     fs.writeFileSync('./dist/links.json', JSON.stringify(doc));
   } catch (e) {
     console.error(e);
